5d5350cd0b47b258671678a26a84ee05772bb15f
Yamami1221 <anak2547@gmail.com>
2023-06-09 01:59:21 +0700

    remove unused variables

--- globalVar.js
+++ globalVar.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 
 try {
     const verifiedDatasPath = './data/verifiedDatas.json';
-    const verifySessionsPath = './data/verifySessions.json';
     const welcomeDatasPath = './data/welcomeDatas.json';
 
     if (!fs.existsSync('./data')) {
@@ -13,10 +12,6 @@ try {
         fs.writeFileSync(verifiedDatasPath, JSON.stringify({}));
     }
 
-    if (!fs.existsSync(verifySessionsPath)) {
-        fs.writeFileSync(verifySessionsPath, JSON.stringify({}));
-    }
-
     if (!fs.existsSync(welcomeDatasPath)) {
         fs.writeFileSync(welcomeDatasPath, JSON.stringify({}));
     }
@@ -24,13 +19,10 @@ try {
     const verifiedDatasRaw = fs.readFileSync(verifiedDatasPath);
     const verifiedDatasParsed = JSON.parse(verifiedDatasRaw);
     const verifiedDatas = new Map(Object.entries(verifiedDatasParsed));
-    const verifySessionsRaw = fs.readFileSync(verifySessionsPath);
-    const verifySessionsParsed = JSON.parse(verifySessionsRaw);
-    const verifySessions = new Map(Object.entries(verifySessionsParsed));
     const welcomeDatasRaw = fs.readFileSync(welcomeDatasPath);
     const welcomeDatasParsed = JSON.parse(welcomeDatasRaw);
     const welcomeDatas = new Map(Object.entries(welcomeDatasParsed));
-    module.exports = { verifiedDatas, verifySessions, welcomeDatas, verifiedDatasPath, verifySessionsPath, welcomeDatasPath };
+    module.exports = { verifiedDatas, welcomeDatas, verifiedDatasPath, welcomeDatasPath };
 } catch (error) {
     console.error(error);
 }
