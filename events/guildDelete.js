b16065d8aaefa98bef1d8a4edb379ecece856480
Yamami1221 <anak2547@gmail.com>
2023-06-16 00:31:09 +0700

    remove send message from guildDelete event

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,4 +1,4 @@
-const { Events, EmbedBuilder } = require('discord.js');
+const { Events } = require('discord.js');
 const fs = require('fs');
 
 const { musicPlayerDatas, musicPlayerDatasPath } = require('../globalVar');
@@ -6,10 +6,6 @@ const { musicPlayerDatas, musicPlayerDatasPath } = require('../globalVar');
 module.exports = {
     name: Events.GuildDelete,
     async execute(guild) {
-        const embed = new EmbedBuilder()
-            .setTitle('👋Bye!')
-            .setDescription('Bye! Thanks for having me in your server!\nHope to see you again!');
-        await guild.systemChannel.send({ embeds: [embed] });
         musicPlayerDatas.delete(guild.id);
         const guildMusicPlayerDataObject = Object.fromEntries(musicPlayerDatas);
         fs.writeFileSync(musicPlayerDatasPath, JSON.stringify(guildMusicPlayerDataObject, null, 4));
