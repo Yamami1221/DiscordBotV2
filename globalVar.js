360d466a8732e963c85349467e5de1513b13e5ff
Yamami1221 <anak2547@gmail.com>
2023-06-15 21:41:26 +0700

    add musicPlayerDatas to globalVar

--- globalVar.js
+++ globalVar.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 
 try {
+    const musicPlayerDatasPath = './data/musicPlayerDatas.json';
     const verifiedDatasPath = './data/verifiedDatas.json';
     const welcomeDatasPath = './data/welcomeDatas.json';
 
     if (!fs.existsSync('./data')) {
         fs.mkdirSync('./data');
+    }
+
+    if (!fs.existsSync(musicPlayerDatasPath)) {
+        fs.writeFileSync(musicPlayerDatasPath, JSON.stringify({}));
     }
 
     if (!fs.existsSync(verifiedDatasPath)) {
@@ -16,13 +21,16 @@ try {
         fs.writeFileSync(welcomeDatasPath, JSON.stringify({}));
     }
 
+    const musicPlayerDatasRaw = fs.readFileSync(musicPlayerDatasPath);
+    const musicPlayerDatasParsed = JSON.parse(musicPlayerDatasRaw);
+    const musicPlayerDatas = new Map(Object.entries(musicPlayerDatasParsed));
     const verifiedDatasRaw = fs.readFileSync(verifiedDatasPath);
     const verifiedDatasParsed = JSON.parse(verifiedDatasRaw);
     const verifiedDatas = new Map(Object.entries(verifiedDatasParsed));
     const welcomeDatasRaw = fs.readFileSync(welcomeDatasPath);
     const welcomeDatasParsed = JSON.parse(welcomeDatasRaw);
     const welcomeDatas = new Map(Object.entries(welcomeDatasParsed));
-    module.exports = { verifiedDatas, welcomeDatas, verifiedDatasPath, welcomeDatasPath };
+    module.exports = { musicPlayerDatas, verifiedDatas, welcomeDatas, musicPlayerDatasPath, verifiedDatasPath, welcomeDatasPath };
 } catch (error) {
     console.error(error);
 }
