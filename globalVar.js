c3e757cccc75656f6e589bbaeceae58a371dc1e4
Yamami1221 <anak2547@gmail.com>
2023-06-26 12:36:18 +0700

    set yt_token and spotify when load globalVar

--- globalVar.js
+++ globalVar.js
@@ -1,6 +1,28 @@
 const fs = require('fs');
 
 try {
+    const { video_info, stream_from_info, playlist_info, search, validate, setToken, getFreeClientID, is_expired, refreshToken } = require('play-dl');
+    (async () => {
+        const SOclientID = await getFreeClientID();
+        await setToken({
+            youtube: {
+                cookie: process.env.YT_COOKIE,
+            },
+            spotify : {
+                client_id: process.env.SPOTIFY_CLIENT_ID,
+                client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+                refresh_token: process.env.SPOTIFY_REFRESH_TOKEN,
+                market: 'TH',
+            },
+            soundcloud : {
+                client_id: SOclientID,
+            },
+        });
+        if (is_expired()) {
+            await refreshToken();
+        }
+    })();
+
     const musicPlayerDatasPath = './data/musicPlayerDatas.json';
     const verifiedDatasPath = './data/verifiedDatas.json';
     const welcomeDatasPath = './data/welcomeDatas.json';
@@ -21,6 +43,7 @@ try {
         fs.writeFileSync(welcomeDatasPath, JSON.stringify({}));
     }
 
+    const audioPlayerDatas = new Map();
     const musicPlayerDatasRaw = fs.readFileSync(musicPlayerDatasPath);
     const musicPlayerDatasParsed = JSON.parse(musicPlayerDatasRaw);
     const musicPlayerDatas = new Map(Object.entries(musicPlayerDatasParsed));
@@ -30,7 +53,7 @@ try {
     const welcomeDatasRaw = fs.readFileSync(welcomeDatasPath);
     const welcomeDatasParsed = JSON.parse(welcomeDatasRaw);
     const welcomeDatas = new Map(Object.entries(welcomeDatasParsed));
-    module.exports = { musicPlayerDatas, verifiedDatas, welcomeDatas, musicPlayerDatasPath, verifiedDatasPath, welcomeDatasPath };
+    module.exports = { video_info, stream_from_info, playlist_info, validate, search, audioPlayerDatas, musicPlayerDatas, verifiedDatas, welcomeDatas, musicPlayerDatasPath, verifiedDatasPath, welcomeDatasPath };
 } catch (error) {
     console.error(error);
 }
